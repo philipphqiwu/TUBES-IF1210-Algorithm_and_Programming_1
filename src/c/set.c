bbd7c721364211a84726179b8b5711f520fd8737
Shui <faiq.azzam@gmail.com>
2025-05-04 19:00:08 +0700

    feat: add set.c file and the content
    created the implementation of set.h header (not done yet).

---
+++ src/c/set.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <string.h>
+#include "set.h"
+
+void createSet(Set *s, int capacity){
+    s->data = (ElType *)malloc(capacity * sizeof(ElType));
+    s->nEff = 0;
+    s->capacity = capacity;
+}
+
+void freeSet(Set *s){
+    free(s->data);
+    s->nEff = 0;
+    s->capacity = 0;
+}
+
+int setContain(Set *s, ElType data){
+    for(int i = 0; i < s->nEff; i++){
+        if(s->data[i] == data){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int setIndeksOf(Set *s, ElType data){
+    if(setContain(s, data)){
+        for(int i = 0; i < s->nEff; i++){
+            if(s->data[i] == data){
+                return i;
+            }
+        }
+    }
+    else{
+        printf("Tidak ada data terkait.\n");
+    }
+}
+
+void setInsert(Set *s, ElType data){
+    if(!setContain(s, data) && s->nEff < s->capacity){
+        strcpy(s->data[s->nEff], data);
+        s->nEff++;
+    }
+    else if(setContain(s,data)){
+        printf("Data sudah ada.\n");
+    }
+    else{
+        printf("Set penuh.\n");
+    }
+}
+
+void setDelete(Set *s, ElType data){
+    if(setContain(s, data)){
+        strcpy(s->data[s->nEff], data);
+        s->nEff++;
+    }
+    else if(setContain(s,data)){
+        printf("Data sudah ada.\n");
+    }
+    else{
+        printf("Set penuh.\n");
+    }
+}
+
+int setSize(Set *s){
+
+}
--- src/header/set.h
+++ src/header/set.h
@@ -1,25 +1,41 @@
 #ifndef SET_H
 #define SET_H
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 20
 
 typedef char* ElType;
 
 typedef struct {
-    ElType data;
+    ElType *data;
+    int nEff;
+    int capacity;
 } Set;
 
-void insertSet(Set *s, ElType data);
-// I.S. data dan s terdefinisi, tidak ada data bernilai sama dalam set
+void createSet(Set *s, int capacity);
+// I.S. s dan capacity terdefinisi
+// F.S. Set s terbentuk dengan kapasitas capacity dan dapat diisi
+
+void freeSet(Set *s);
+// I.S. s terdefinisi
+// F.S. kapaistas Set s menjadi 0 (tidak dapat diisi)
+
+int setContain(Set *s, ElType data);
+// mengecek apakah set memiliki elemen bernilai data. Mengembalikan 1 jika ada, 0 jika tidak. 
+
+void setInsert(Set *s, ElType data);
+// I.S. data dan s terdefinisi
 // F.S. data terdapat dalam set
 
-void deleteSet(Set *s, ElType data);
+void setDelete(Set *s, ElType data);
 // I.S. data dan s terdefinisi, ada data dalam set
 // F.S. data hilang dari set
 
-int sizeSet(Set *s);
+int setSize(Set *s);
 // mengembalikan banyak elemen dalam set
 
-int containSet(Set *s, ElType data);
+int setContain(Set *s, ElType data);
 // mengecek apakah set memiliki elemen bernilai data. Mengembalikan 1 jika ada, 0 jika tidak. 
 
 #endif
