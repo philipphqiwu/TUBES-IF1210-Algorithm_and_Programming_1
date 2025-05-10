23d151dca676e5470597918d3b61e77b811189f8
Beeziebloop <varistha.devi@gmail.com>
2025-05-10 18:12:24 +0700

    feat: change ADT struct for obat-penyakit.h
    Update untuk ADT obat-penyakit.h dengan menggunakan ADT Map

--- src/header/obat-penyakit.h
+++ src/header/obat-penyakit.h
@@ -1,10 +1,20 @@
 #ifndef OBAT_PENYAKIT_H
 #define OBAT_PENYAKIT_H
+#include "map.h"
 
 typedef struct{
-    int obat_id;
-    int penyakit_id;
+    int id_obat;
     int urutan_minum;
-} ObatPenyakit;
+}Value;
+
+typedef struct {
+    int penyakit_id; //ini key
+    Value value[10];
+}MapPair;
+
+typedef struct {
+    MapPair items[MAX_MAP_SIZE];
+    int size;
+}Map;
 
 #endif
