a55d557d39985d8d1b6c8a7b0d2a5b6624e1f146
Shui <faiq.azzam@gmail.com>
2025-05-10 14:28:44 +0700

    feat: add implementation of parsing of obat.csv and penyakit.csv
    In initialize-program.c, the function inside now will try to get all csv files and put the data into the program (for now it's just obat.csv and penyakit.csv).

--- src/c/initialize-program.c
+++ src/c/initialize-program.c
@@ -2,9 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "initialize-program.h"
+#include "general-parsing.h"
+#include "user.h"
+#include "obat.h"
+#include "penyakit.h"
+#include "obat-penyakit.h"
 
-int initializeProgram(){
+int initializeProgram(ListObat *listObat, ListPenyakit *ListPenyakit){
     printf("===============\n");
     printf("SELAMAT DATANG\n");
     printf("===============\n");
+
+    FILE *userFile = fopen("data/obat.csv", "r");
+    FILE *obatFile = fopen("data/obat.csv", "r");
+    FILE *penyakitFile = fopen("data/obat.csv", "r");
+    FILE *obatPenyakitFile = fopen("data/obat.csv", "r");
+    
+    // Parsing dan pemasukan data obat
+    char lineInput[1000];
+    fgets(lineInput, sizeof(lineInput), obatFile);
+    while(fgets(lineInput, sizeof(lineInput), obatFile)){
+        Obat item;
+        parsing(lineInput, "is", 2, &item.obat_id, item.nama_obat);
+    }
+    while(fgets(lineInput, sizeof(lineInput), penyakitFile)){
+        Penyakit item;
+        parsing(lineInput, "isffiiiiiiffiiffiiiiii", 22, &item.id, item.nama_penyakit, &item.suhu_tubuh_min, &item.suhu_tubuh_max, &item.tekanan_darah_sistolik_min, &item.tekanan_darah_sistolik_max, &item.tekanan_darah_diastolik_min, &item.tekanan_darah_diastolik_max, &item.detak_jantung_min, &item.detak_jantung_max, &item.kadar_gula_darah_min, &item.kadar_gula_darah_max, &item.berat_badan_min, &item.berat_badan_max, &item.berat_badan_min, &item.berat_badan_max, &item.tinggi_badan_min, &item.tinggi_badan_max, &item.kadar_kolesterol_min, &item.kadar_kolesterol_max, &item.trombosit_min, &item.trombosit_max);
+    }
 }
