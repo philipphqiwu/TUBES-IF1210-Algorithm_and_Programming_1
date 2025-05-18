c332ad54d14b9f165774156a12d1b1e0e13cc7ff
Luis <nicholasluischandra@gmail.com>
2025-05-18 14:36:38 +0700

    fix : read-config

--- src/c/parse-config.c
+++ src/c/parse-config.c
@@ -2,23 +2,28 @@
 #include "../header/user.h"
 #include "../header/config.h"
 
-void readConfig(Config *rumahsakit, ListDinUser * UserData) {
+
+void readConfig(Config *rumahsakit, ListDinUser *UserData) {
     FILE *file = fopen("../data/config.txt", "r");
     if (!file) {
         printf("Gagal membuka file.\n");
         return;
     }
 
-    fscanf(file, "%d %d", &rumahsakit->roomRow, &rumahsakit->roomCol);
+    char line[256];
+    char *ptr, *end;
 
+    fgets(line, sizeof(line), file);
+    rumahsakit->roomRow = (int)strtol(line, &end, 10);
+    rumahsakit->roomCol = (int)strtol(end, &end, 10);
 
-    fscanf(file, "%d", &rumahsakit->roomCapacity);
-    int c;
-    while ((c = fgetc(file)) != '\n' && c != EOF); 
+    fgets(line, sizeof(line), file);
+    rumahsakit->roomCapacity = (int)strtol(line, &end, 10);
 
-    char buffer[256];
+    while(fgetc(file) != '\n');
+
     for (int i = 0; i < rumahsakit->roomRow * rumahsakit->roomCol; i++) {
-        if (fgets(buffer, sizeof(buffer), file) == NULL) {
+        if (!fgets(line, sizeof(line), file)) {
             printf("File tidak lengkap!\n");
             break;
         }
@@ -27,61 +32,105 @@ void readConfig(Config *rumahsakit, ListDinUser * UserData) {
             rumahsakit->room[i][j] = 0;
         }
 
-        char *token = strtok(buffer, " ");
+        ptr = line;
         int counter = 0;
         char kodeRuang[3];
         int row = i / rumahsakit->roomCol;
         int col = i % rumahsakit->roomCol;
-        while (token != NULL && counter <= rumahsakit->roomCapacity) {
-            if(counter == 0){
-                sprintf(kodeRuang, "%c%c", 'A' + row, '1' + col);
-                strcpy(UserData->buffer[atoi(token)-1].ruang,kodeRuang);
+        
+        while (*ptr && counter <= rumahsakit->roomCapacity) {
+
+            while (*ptr == ' ') ptr++;
+            
+
+            long val = strtol(ptr, &end, 10);
+            if (ptr == end) break;
+            
+            if (counter == 0) {
+
+                snprintf(kodeRuang, sizeof(kodeRuang), "%c%c", 'A' + row, '1' + col);
+                
+
+                strncpy(UserData->buffer[val-1].ruang, kodeRuang, sizeof(UserData->buffer[0].ruang)-1);
             }
-            rumahsakit->room[i][counter++] = atoi(token)-1;
-            token = strtok(NULL, " ");
+            
+            rumahsakit->room[i][counter++] = (int)val - 1;
+            ptr = end;
         }
     }
 
-    fscanf(file, "%d", &rumahsakit->jumlahPemilikobat);
+
+    fgets(line, sizeof(line), file);
+    rumahsakit->jumlahPemilikobat = (int)strtol(line, &end, 10);
+
+
     for (int i = 0; i < rumahsakit->jumlahPemilikobat; i++) {
+        if (!fgets(line, sizeof(line), file)) break;
+        
+        ptr = line;
         for (int j = 0; j < 6; j++) {
-            fscanf(file, "%d", &rumahsakit->inventoryPasien[i][j]);
+      
+            while (*ptr == ' ') ptr++;
+            
+            long val = strtol(ptr, &end, 10);
+            rumahsakit->inventoryPasien[i][j] = (int)val;
+            ptr = end;
         }
     }
 
     fclose(file);
 }
-// void writeConfig(const Config *rs) {
-//     FILE *file = fopen("config.txt", "w");
+
+
+
+
+
+// void readConfig(Config *rumahsakit, ListDinUser * UserData) {
+//     FILE *file = fopen("../data/config.txt", "r");
 //     if (!file) {
-//         printf("Gagal membuat/membuka file.\n");
+//         printf("Gagal membuka file.\n");
 //         return;
 //     }
 
-//     fprintf(file, "%d %d\n", rs->roomRow, rs->roomCol);
+//     fscanf(file, "%d %d", &rumahsakit->roomRow, &rumahsakit->roomCol);
 
-//     fprintf(file, "%d\n", rs->roomCapacity);
 
-//     for (int i = 0; i < rs->roomRow * rs->roomCol; i++) {
-//         int j = 0;
-//         while (fprintf(file, "%d\n",  rs->room[i][j]) != \n){
-//             j++
+//     fscanf(file, "%d", &rumahsakit->roomCapacity);
+//     int c;
+//     while ((c = fgetc(file)) != '\n' && c != EOF); 
+
+//     char buffer[256];
+//     for (int i = 0; i < rumahsakit->roomRow * rumahsakit->roomCol; i++) {
+//         if (fgets(buffer, sizeof(buffer), file) == NULL) {
+//             printf("File tidak lengkap!\n");
+//             break;
+//         }
+
+//         for (int j = 0; j < 100; j++) {
+//             rumahsakit->room[i][j] = 0;
+//         }
+
+//         char *token = strtok(buffer, " ");
+//         int counter = 0;
+//         char kodeRuang[3];
+//         int row = i / rumahsakit->roomCol;
+//         int col = i % rumahsakit->roomCol;
+//         while (token != NULL && counter <= rumahsakit->roomCapacity) {
+//             if(counter == 0){
+//                 sprintf(kodeRuang, "%c%c", 'A' + row, '1' + col);
+//                 strcpy(UserData->buffer[atoi(token)-1].ruang,kodeRuang);
+//             }
+//             rumahsakit->room[i][counter++] = atoi(token)-1;
+//             token = strtok(NULL, " ");
 //         }
 //     }
 
-    
-//     fprintf(file, "%d\n", rs->jumlahPemilikobat);
-
-    
-//     for (int i = 0; i < rs->jumlahPemilikobat; i++) {
-//         fprintf(file, "%d %d %d %d %d %d\n",
-//                 rs->inventoryPasien[i][0],
-//                 rs->inventoryPasien[i][1],
-//                 rs->inventoryPasien[i][2],
-//                 rs->inventoryPasien[i][3],
-//                 rs->inventoryPasien[i][4],
-//                 rs->inventoryPasien[i][5]);
+//     fscanf(file, "%d", &rumahsakit->jumlahPemilikobat);
+//     for (int i = 0; i < rumahsakit->jumlahPemilikobat; i++) {
+//         for (int j = 0; j < 6; j++) {
+//             fscanf(file, "%d", &rumahsakit->inventoryPasien[i][j]);
+//         }
 //     }
 
-//     fclose(file); 
+//     fclose(file);
 // }
