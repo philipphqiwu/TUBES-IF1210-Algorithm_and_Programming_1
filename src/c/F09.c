add2234a994e6cf0899a98a168c47841c057d25b
Beeziebloop <varistha.devi@gmail.com>
2025-05-21 15:11:19 +0700

    feat: update F09
    Update code untuk F09.c (masih on going)

--- src/c/F09.c
+++ src/c/F09.c
@@ -47,10 +47,35 @@ void lihatAntrian(Config rumahsakit, ListDinUser UserData){
         printf("\n");
     }
 
-    char koderuangan[100];
-    int baris = koderuangan[0] - 'A';
-    int kolom = atoi(koderuangan + 1) - 1;
-    int indeks = baris * rumahsakit.roomCol + kolom;
+    for(int baris = 0; baris < rumahsakit.roomRow; baris++){
+        for(int kolom = 0; kolom < rumahsakit.roomCol; kolom++){
+
+            char koderuangan[3];
+            koderuangan[0] = 'A' + baris;
+            koderuangan[1] = '1' + kolom;
+            koderuangan[2] = '\0';
+            
+            int indeks = baris * rumahsakit.roomCol + kolom;
+
+            int id_dokter = rumahsakit.room[indeks][0];
+            if(id_dokter != 0){
+                printf("\n============ %s ============\n", koderuangan);
+                printf("Kapasitas  : %d\n", rumahsakit.roomCapacity);
+                printf("Dokter     : %s\n", (id_dokter == 0) ? "-" : cari_username(UserData, id_dokter+1));
+
+                printf("Pasien di dalam ruangan:\n");
+                int pasien_count = 0;
+                for (int i = 1; i <= rumahsakit.roomCapacity; i++) {
+                    int id_pasien = rumahsakit.room[indeks][i];
+                    if (id_pasien != 0) {
+                        printf("  %d. %s\n", ++pasien_count, cari_username(UserData, id_pasien+1));
+                    }
+                }
+                if (pasien_count == 0) printf("  Tidak ada pasien\n");
 
 
+                printf("------------------------------\n");
+            }
+        }
+    }
 }
