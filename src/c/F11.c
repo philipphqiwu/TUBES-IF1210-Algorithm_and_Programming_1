292453e9ee6cbddf9cac6efd31177026d369fecb
Luis <nicholasluischandra@gmail.com>
2025-05-30 18:07:11 +0700

    fix : F11 parameter

--- src/c/F11.c
+++ src/c/F11.c
@@ -7,68 +7,68 @@
 
 
 // Merupakan fungsi untuk mengecek apakah pasien terkena suatu penyakit
-int cekPenyakit(ListPenyakit kriteriapenyakit, ListDinUser* UserData, int idPasien){
+int cekPenyakit(ListPenyakit kriteriaPenyakit, ListDinUser UserData, int idPasien){
     for (int i = 0; i <= 5; i++) {
        
         boolean allMatch = true;
         
         // 1. Pengecekan Suhu tubuh
-        if (UserData->buffer[idPasien].suhu_tubuh < kriteriapenyakit.items[i].suhu_tubuh_min || 
-            UserData->buffer[idPasien].suhu_tubuh > kriteriapenyakit.items[i].suhu_tubuh_max) {
+        if (UserData.buffer[idPasien].suhu_tubuh < kriteriaPenyakit.items[i].suhu_tubuh_min || 
+            UserData.buffer[idPasien].suhu_tubuh > kriteriaPenyakit.items[i].suhu_tubuh_max) {
             allMatch = false;
         }
         
         // 2. Pengecekan Tekanan darah sistolik
-        if (allMatch && (UserData->buffer[idPasien].tekanan_darah_sistolik < kriteriapenyakit.items[i].tekanan_darah_sistolik_min || 
-            UserData->buffer[idPasien].tekanan_darah_sistolik > kriteriapenyakit.items[i].tekanan_darah_sistolik_max)) {
+        if (allMatch && (UserData.buffer[idPasien].tekanan_darah_sistolik < kriteriaPenyakit.items[i].tekanan_darah_sistolik_min || 
+            UserData.buffer[idPasien].tekanan_darah_sistolik > kriteriaPenyakit.items[i].tekanan_darah_sistolik_max)) {
             allMatch = false;
         }
         
         // 3. Pengecekan Tekanan darah diastolik
-        if (allMatch && (UserData->buffer[idPasien].tekanan_darah_diastolik < kriteriapenyakit.items[i].tekanan_darah_diastolik_min || 
-            UserData->buffer[idPasien].tekanan_darah_diastolik > kriteriapenyakit.items[i].tekanan_darah_diastolik_max)) {
+        if (allMatch && (UserData.buffer[idPasien].tekanan_darah_diastolik < kriteriaPenyakit.items[i].tekanan_darah_diastolik_min || 
+            UserData.buffer[idPasien].tekanan_darah_diastolik > kriteriaPenyakit.items[i].tekanan_darah_diastolik_max)) {
             allMatch = false;
         }
         
         // 4. Pencekan Detak jantung
-        if (allMatch && (UserData->buffer[idPasien].detak_jantung < kriteriapenyakit.items[i].detak_jantung_min || 
-            UserData->buffer[idPasien].detak_jantung > kriteriapenyakit.items[i].detak_jantung_max)) {
+        if (allMatch && (UserData.buffer[idPasien].detak_jantung < kriteriaPenyakit.items[i].detak_jantung_min || 
+            UserData.buffer[idPasien].detak_jantung > kriteriaPenyakit.items[i].detak_jantung_max)) {
             allMatch = false;
         }
         
         // 5. Pengecekan Saturasi oksigen
-        if (allMatch && (UserData->buffer[idPasien].saturasi_oksigen < kriteriapenyakit.items[i].saturasi_oksigen_min || 
-            UserData->buffer[idPasien].saturasi_oksigen > kriteriapenyakit.items[i].saturasi_oksigen_max)) {
+        if (allMatch && (UserData.buffer[idPasien].saturasi_oksigen < kriteriaPenyakit.items[i].saturasi_oksigen_min || 
+            UserData.buffer[idPasien].saturasi_oksigen > kriteriaPenyakit.items[i].saturasi_oksigen_max)) {
             allMatch = false;
         }
         
         // 6. Pengecekan Kadar gula darah
-        if (allMatch && (UserData->buffer[idPasien].kadar_gula_darah < kriteriapenyakit.items[i].kadar_gula_darah_min || 
-            UserData->buffer[idPasien].kadar_gula_darah > kriteriapenyakit.items[i].kadar_gula_darah_max)) {
+        if (allMatch && (UserData.buffer[idPasien].kadar_gula_darah < kriteriaPenyakit.items[i].kadar_gula_darah_min || 
+            UserData.buffer[idPasien].kadar_gula_darah > kriteriaPenyakit.items[i].kadar_gula_darah_max)) {
             allMatch = false;
         }
         
         // 7. Pengecekan Berat badan
-        if (allMatch && (UserData->buffer[idPasien].berat_badan < kriteriapenyakit.items[i].berat_badan_min || 
-            UserData->buffer[idPasien].berat_badan > kriteriapenyakit.items[i].berat_badan_max)) {
+        if (allMatch && (UserData.buffer[idPasien].berat_badan < kriteriaPenyakit.items[i].berat_badan_min || 
+            UserData.buffer[idPasien].berat_badan > kriteriaPenyakit.items[i].berat_badan_max)) {
             allMatch = false;
         }
         
         // 8. Pengecekan Tinggi badan
-        if (allMatch && (UserData->buffer[idPasien].tinggi_badan < kriteriapenyakit.items[i].tinggi_badan_min || 
-            UserData->buffer[idPasien].tinggi_badan > kriteriapenyakit.items[i].tinggi_badan_max)) {
+        if (allMatch && (UserData.buffer[idPasien].tinggi_badan < kriteriaPenyakit.items[i].tinggi_badan_min || 
+            UserData.buffer[idPasien].tinggi_badan > kriteriaPenyakit.items[i].tinggi_badan_max)) {
             allMatch = false;
         }
         
         // 9. Pengecekan Kadar kolesterol
-        if (allMatch && (UserData->buffer[idPasien].kadar_kolesterol < kriteriapenyakit.items[i].kadar_kolesterol_min || 
-            UserData->buffer[idPasien].kadar_kolesterol > kriteriapenyakit.items[i].kadar_kolesterol_max)) {
+        if (allMatch && (UserData.buffer[idPasien].kadar_kolesterol < kriteriaPenyakit.items[i].kadar_kolesterol_min || 
+            UserData.buffer[idPasien].kadar_kolesterol > kriteriaPenyakit.items[i].kadar_kolesterol_max)) {
             allMatch = false;
         }
         
         // 10. Pngecekan Trombosit
-        if (allMatch && (UserData->buffer[idPasien].trombosit < kriteriapenyakit.items[i].trombosit_min || 
-            UserData->buffer[idPasien].trombosit > kriteriapenyakit.items[i].trombosit_max)) {
+        if (allMatch && (UserData.buffer[idPasien].trombosit < kriteriaPenyakit.items[i].trombosit_min || 
+            UserData.buffer[idPasien].trombosit > kriteriaPenyakit.items[i].trombosit_max)) {
             allMatch = false;
         }
         
@@ -83,10 +83,10 @@ int cekPenyakit(ListPenyakit kriteriapenyakit, ListDinUser* UserData, int idPasi
 }
 
 // Melalukan assign penyakit ketika telah dicek penyakitnya melalui cekPenyakit
-boolean assignPenyakit(ListPenyakit kriteriapenyakit, ListDinUser* UserData, int idPasien){
-    int kondisipasien = cekPenyakit(kriteriapenyakit, UserData, idPasien);
+boolean assignPenyakit(ListPenyakit kriteriaPenyakit, ListDinUser* UserData, int idPasien){
+    int kondisipasien = cekPenyakit(kriteriaPenyakit, *UserData, idPasien);
     if (kondisipasien != -1){
-        strcpy(UserData->buffer[idPasien].riwayat_penyakit, kriteriapenyakit.items[kondisipasien].nama_penyakit);
+        strcpy(UserData->buffer[idPasien].riwayat_penyakit, kriteriaPenyakit.items[kondisipasien].nama_penyakit);
         return true;
     }
     else{
@@ -119,7 +119,7 @@ int diagnosisPenyakit (ListPenyakit krirteriapenyakit, Config rumahsakit, ListDi
         // validasi dari pasien terdepan yang akan diobati
         int idxPasien = cariIdxUser(UserData, rumahsakit.denah.contents[idxBaris][idxKolom].antrian->front->data);
         if (idxPasien != -1){
-            int idPenyakitSekarang = cekPenyakit(krirteriapenyakit, UserData, idxPasien);
+            int idPenyakitSekarang = cekPenyakit(krirteriapenyakit, *UserData, idxPasien);
             // Kemungkinan 1 : pasien pernah didiagnosis
             if (punyaRiwayat(UserData->buffer[idxPasien].riwayat_penyakit)) {
                 // Jika pasien sudah sembuh
