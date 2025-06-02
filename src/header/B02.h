081e52be31c3e92c2bb1364db6bf88e98b161d89
Shui <faiq.azzam@gmail.com>
2025-06-02 08:45:44 +0700

    style: add comment on B02.h

--- src/header/B02.h
+++ src/header/B02.h
@@ -2,4 +2,10 @@
 #include "config.h"
 
 void ubahDenah(Config *rumahSakit);
+// I.S. rumahsakit terdefinisi
+// F.S. Mengambil inputan user berupa 2 integer dan mengganti konfigurasi ruangan berdasarkan inputan user jika ruangan yang akan dihilangkan kosong
+
+
 void pindahDokter(ListDinUser userData, Config *rumahSakit);
+// I.S. userData dan rumahsakit terdefinisi
+// F.S. Mengambil inputan user berubah 2 integer dan memindahkan isi ruangan awal ke ruangan tujuan jika ruangan tujuan kosong
